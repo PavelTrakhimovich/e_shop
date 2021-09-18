a9fbd4fdb4f08d25a98a384b35fb8cd8cedcfea1
Pavel Trakhimovich <ptrakhimovich@gmail.com>
2021-09-18 17:03:03 +0300

    Some products in model

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,10 +1,11 @@
-from django import forms 
+
+from django.forms import ModelChoiceField
 from django.contrib import admin
 from .models import *
 
 
-class NotebookCategoryChoiceField(forms.ModelChoiceField):
-    pass
+# class NotebookCategoryChoiceField(forms.ModelChoiceField):
+#     pass
 
 
 class NotebookAdmin(admin.ModelAdmin):
@@ -12,12 +13,12 @@ class NotebookAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug='notebooks'))
+            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
-class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
-    pass
+# class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
+#     pass
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
@@ -25,7 +26,7 @@ class SmartphoneAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return SmartphoneCategoryChoiceField(Category.objects.filter(slug='smartphones'))
+            return ModelChoiceField(Category.objects.filter(slug='smartphones'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(Category)
